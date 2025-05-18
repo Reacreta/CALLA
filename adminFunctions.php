8cf4f6680091ddfe92f102bd75ac5f61f20f9f7d
t3riteki <gobythebox@gmail.com>
2025-05-18 17:14:01 +0800

    Update adminFunctions.php

--- adminFunctions.php
+++ adminFunctions.php
@@ -1,61 +1,74 @@
 <?php
-require_once 'database.php';
+  ob_start();
+  session_start();
+  require_once 'database.php';
+  require_once 'authFunctions.php';
 
-$type = $_POST['type'] ?? 'All';
+  $type = $_POST['type'] ?? 'All';
 
-if ($type === 'Partner') {
-    $sql = "SELECT p.partnerName AS 'uploader' , lm.moduleName, lm.langID FROM partnermodule pm 
-            JOIN partner p ON p.partnerID = pm.partnerID 
-            JOIN languagemodule lm on lm.langID = pm.langID;";
+  // Load the modules based on the type
+  if ($type === 'Partner') {
+      $sql = "SELECT p.partnerName AS 'uploader' , lm.moduleName, lm.langID FROM partnermodule pm 
+              JOIN partner p ON p.partnerID = pm.partnerID 
+              JOIN languagemodule lm on lm.langID = pm.langID;";
 
-} elseif ($type === 'Classroom') {
-    $sql = " SELECT 
-              lm.langID, 
-              lm.moduleName, 
-              u.username AS 'uploader'
-            FROM classmodule cm 
-            JOIN classinstructor ci ON cm.classInstID = ci.classInstID
-            JOIN instructor i ON i.instID = ci.instID
-            JOIN users u ON u.userID = i.userID
-            JOIN languagemodule lm ON lm.langID = cm.langID;";
-} else {
-    $sql = "
-      SELECT 
-        p.partnerName AS 'uploader' , 
-        lm.moduleName, 
-        lm.langID 
-      FROM partnermodule pm 
-      JOIN partner p ON p.partnerID = pm.partnerID 
-      JOIN languagemodule lm on lm.langID = pm.langID
+  } elseif ($type === 'Classroom') {
+      $sql = " SELECT 
+                lm.langID, 
+                lm.moduleName, 
+                u.username AS 'uploader'
+              FROM classmodule cm 
+              JOIN classinstructor ci ON cm.classInstID = ci.classInstID
+              JOIN instructor i ON i.instID = ci.instID
+              JOIN users u ON u.userID = i.userID
+              JOIN languagemodule lm ON lm.langID = cm.langID;";
+  } else {
+      $sql = "
+        SELECT 
+          p.partnerName AS 'uploader' , 
+          lm.moduleName, 
+          lm.langID 
+        FROM partnermodule pm 
+        JOIN partner p ON p.partnerID = pm.partnerID 
+        JOIN languagemodule lm on lm.langID = pm.langID
 
-      UNION
+        UNION
 
-      SELECT 
-        u.username AS 'uploader',
-        lm.moduleName, 
-        lm.langID 
-      FROM classmodule cm 
-      JOIN classinstructor ci ON cm.classInstID = ci.classInstID
-      JOIN instructor i ON i.instID = ci.instID
-      JOIN users u ON u.userID = i.userID
-      JOIN languagemodule lm ON lm.langID = cm.langID;
-    ";
-}
+        SELECT 
+          u.username AS 'uploader',
+          lm.moduleName, 
+          lm.langID 
+        FROM classmodule cm 
+        JOIN classinstructor ci ON cm.classInstID = ci.classInstID
+        JOIN instructor i ON i.instID = ci.instID
+        JOIN users u ON u.userID = i.userID
+        JOIN languagemodule lm ON lm.langID = cm.langID;
+      ";
+  }
 
-$result = $conn->query($sql);
-while ($row = $result->fetch_assoc()) {
-?>
-  <div class="module-card">
-    <img src="images/Module_Icon.jpg" alt="Module Icon" class="module-icon">
-    <div class="module-info">
-      <div class="module-title"><?= htmlspecialchars($row['moduleName']) ?></div>
-      <div class="module-creator">By <?= htmlspecialchars($row['uploader']) ?></div>
+  $result = $conn->query($sql);
+  while ($row = $result->fetch_assoc()) {
+  ?>
+    <div class="module-card">
+      <img src="images/Module_Icon.jpg" alt="Module Icon" class="module-icon">
+      <div class="module-info">
+        <div class="module-title"><?= htmlspecialchars($row['moduleName']) ?></div>
+        <div class="module-creator">By <?= htmlspecialchars($row['uploader']) ?></div>
+      </div>
+        <button onclick="">
+          <img src="images/Search_Icon.jpg" alt="View Module" class="search-image-icon">
+        </button>
+      </a>
     </div>
-      <button onclick="">
-        <img src="images/Search_Icon.jpg" alt="View Module" class="search-image-icon">
-      </button>
-    </a>
-  </div>
-<?php
-}
+  <?php
+  }
+
+  function logAction($conn, $userID, $action) {
+    $logID = generateID('L',9);
+
+    $sql = "INSERT INTO activity (logID,userID,action,dateTimeCreated) VALUES (?,?,?,CURRENT_TIMESTAMP)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('sss', $logID, $userID, $action);
+    $stmt->execute();
+  }
 ?>
